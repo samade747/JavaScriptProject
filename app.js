948b855e40773afa173d071d17a3650c91b428db
samade747 <samad.e747@gmail.com>
2023-10-22 21:04:10 +0500

    42

--- app.js
+++ app.js
@@ -2,6 +2,9 @@
 
 function Next() {
     let nextbox = document.getElementById('nextbox'); 
+    let borderbox = document.getElementById('borderbox');
     nextbox.style.opacity = (nextbox.style.opacity === '0') ? '1' : '0';
+    borderbox.style.opacity = (borderbox.style.opacity === '1') ? '0' : '1';
+
    
 }
