eac361604baec752a8d4197adef3561cbecc5188
samade747 <samad.e747@gmail.com>
2023-10-23 18:51:00 +0500

    474858584

--- app.js
+++ app.js
@@ -2,3 +2,9 @@ function Next() {
     var nextbox = document.getElementById('nextbox');
     nextbox.style.display = (nextbox.style.display === 'none') ? 'block' : 'none';
 }
+
+
+function katti(){
+    var nextbox = document.getElementById('nextbox');
+    nextbox.style.display = 'none';
+}
